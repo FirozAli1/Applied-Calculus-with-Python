8da18e399126708f3f80f60f9ca536312665f257
FirozAli1 <61276097+FirozAli1@users.noreply.github.com>
2022-12-17 10:18:39 +0100

    Exercise about a function that takes three numbers
    A function that takes three numbers and add them up then returns the result

---
+++ Python_Intro.py
@@ -0,0 +1,8 @@
+def add_three_number(num1, num2, num3):
+    sum_of_three_number = num1 + num2 + num3
+    return sum_of_three_number
+
+
+# Function "add_three_number" takes 3 numbers and add them up then return the result
+answer = add_three_number(5, 10, 20)
+print("Answer : ", answer)
