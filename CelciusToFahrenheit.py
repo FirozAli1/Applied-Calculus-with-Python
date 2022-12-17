bf0fa01c96574953bdb20abe322923eea6123e82
FirozAli1 <61276097+FirozAli1@users.noreply.github.com>
2022-12-17 10:41:15 +0100

    Celsius to Fahrenheit function
    A handy function that takes temperature in  Celsius metric system and convert that into Fahrenheit metric system.

---
+++ CelciusToFahrenheit.py
@@ -0,0 +1,10 @@
+def celsius_to_fahrenheit(celcius):
+    fahrenheit = 9 / 5 * celcius + 32
+    return fahrenheit
+
+
+fahrenheit = celsius_to_fahrenheit(0)
+print(fahrenheit)
+
+fahrenheit = celsius_to_fahrenheit(100)
+print(fahrenheit)
